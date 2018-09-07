0672ac2ea436f062d025436420618bb27e3a31ce
Kenya <email@email.com>
2018-09-06 19:08:02 -0500

    add program name banner

--- camelCase.py
+++ camelCase.py
@@ -1,8 +1,30 @@
-# create function to join words,
-# capitalize first letter of each word,
-# and make following letters lowercase
-def camelCase(word):
-    output = ''.join(x for x in word.title() if x.isalnum())
-    return output[0].lower() + output[1:]
 
-print(camelCase('sAN fraNCISCO'))
+
+def camelcase(word):
+    # Convert word to have uppercase first letter, rest in lowercase
+    return word[0:1].upper() + word[1:].lower()
+    # Slices don't produce index out of bounds errors.
+    # So this still works on empty strings, strings of length 1
+
+
+def display_banner():
+    # display program name in a banner
+    msg = "AWESOME camelCaseGenerator PROGRAM"
+    stars = "*" * len(msg)
+    print('\n', stars, '\n', msg, '\n', stars, '\n')
+
+
+def main():
+
+    display_banner()
+
+    sentence = input('Enter your sentence:  ')
+    words = sentence.split(' ')                               # Break by spaces
+    camelcased_words = [ camelcase(word) for word in words ]  # camelCase everything
+    camelcased_words[0] = camelcased_words[0].lower()         # lowercase first word
+    output = ''.join(camelcased_words)                        #Put words back together
+    print(output)
+
+
+if __name__ == '__main__':
+    main()
